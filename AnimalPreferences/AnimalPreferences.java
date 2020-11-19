02850b51b6e95f0e445a78b084e847aaab7c0525
Yngrid Corrales <47527313+ylcorral@users.noreply.github.com>
2020-11-18 19:26:08 -0500

    Add files via upload

---
+++ AnimalPreferences/AnimalPreferences.java
@@ -0,0 +1,18 @@
+
+package CSC340.ChangePreferance;
+
+/**
+ *
+ * @author Liz
+ */
+public class AnimalPreferences {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BarnyardPreferencesView theView = new BarnyardPreferencesView();
+        theView.setVisible(true);
+
+    }
+
+}
